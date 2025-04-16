879141104590a8a41aa50bbce0d378ccf0b965fa
OloSrolo <klap.aleksander@gmail.com>
2025-04-16 14:39:44 +0200

    prisma integration

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,24 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
-import { json } from 'stream/consumers';
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import prisma from "./prisma";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Welcome to the WC Quest Server!' });
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to the WC Quest Server!" });
 });
 
 // Health endpoint
-app.get('/health', (req: Request, res: Response) => {
-  res.json({ status: 'healthy' });
+app.get("/health", (req: Request, res: Response) => {
+  res.json({ status: "healthy" });
+});
+
+app.get("/users", async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 app.listen(port, () => {
