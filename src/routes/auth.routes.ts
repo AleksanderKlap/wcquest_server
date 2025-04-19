9e4dc6d95807e33c4a025fa9a25889d9e969b676
OloSrolo <klap.aleksander@gmail.com>
2025-04-19 11:15:29 +0200

    innit swagger setup

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -9,9 +9,30 @@ import { validate } from "../middleware/validate";
 import passport from "../config/passport";
 
 const router = express.Router();
-
+/**
+ * @openapi
+ * /register:
+ *  post:
+ *    tags:
+ *      - Register
+ *    summary: Endpoint to register user with email and password
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/RegisterInput'
+ *    responses:
+ *      201:
+ *        description: Registration succesfull
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/RegisterSuccessResponse'
+ *      400:
+ *        description: Email is already in use OR email is not in email format OR password is less than 6 characters OR password do not contain at least 1 uppercase letter OR password do not contain at least 1 number
+ */
 router.post("/register", registerSchema, validate, register);
-router.post("/register", register);
 router.get("/auth/google", googleAuth);
 router.get(
   "/auth/google/callback",
