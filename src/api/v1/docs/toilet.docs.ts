652cb648384256939ad08aa7eafdb73cf9158da5
OloSrolo <klap.aleksander@gmail.com>
2025-06-02 19:07:33 +0200

    fixed comment docs

--- src/api/v1/docs/toilet.docs.ts
+++ src/api/v1/docs/toilet.docs.ts
@@ -3,6 +3,7 @@ import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
 import { registry } from "./registry.docs";
 import {
   boundingBoxRequest,
+  commentRequest,
   createToiletRequest,
   inRadiusRequest,
   ratingRequest,
@@ -14,6 +15,7 @@ import {
 } from "../schemas/toilet/toilet.response.schema";
 import {
   avgToiletRatings,
+  toiletComment,
   toiletPhoto,
   userRatingReturn,
 } from "../schemas/toilet/toilet.entity.schema";
@@ -282,6 +284,118 @@ registry.registerPath({
           schema: avgToiletRatings,
         },
       },
+    },
+    500: {
+      description: "Something went wrong",
+    },
+  },
+});
+
+registry.registerPath({
+  method: "post",
+  path: "/api/v1/toilet/{id}/comments",
+  tags: ["Toilet"],
+  security: [{ bearerAuth: [] }],
+  description: "Insert new comment for the toilet",
+  request: {
+    params: z.object({ id: z.string() }),
+    body: {
+      content: {
+        "application/json": {
+          schema: commentRequest,
+        },
+      },
+    },
+  },
+  responses: {
+    201: {
+      description: "Comment adding succesfull",
+      content: {
+        "application/json": {
+          schema: toiletComment,
+        },
+      },
+    },
+    401: {
+      description: "Token Expired",
+    },
+    403: {
+      description: "Invalid token",
+    },
+    400: {
+      description: "Wrong body, read the error code",
+    },
+    500: {
+      description: "Something went wrong",
+    },
+  },
+});
+
+registry.registerPath({
+  method: "get",
+  path: "/api/v1/toilet/{id}/comments",
+  tags: ["Toilet"],
+  request: {
+    params: z.object({ id: z.string() }),
+  },
+  description: "Get all comments of toilet ID from freshest to oldest",
+  responses: {
+    200: {
+      description: "Getting succesfull",
+      content: {
+        "application/json": {
+          schema: z.array(toiletComment),
+        },
+      },
+    },
+    500: {
+      description: "Something went wrong",
+    },
+  },
+});
+
+registry.registerPath({
+  method: "get",
+  path: "/api/v1/toilet/comments/my",
+  tags: ["Toilet"],
+  security: [{ bearerAuth: [] }],
+  description: "Get all comments of user ID from freashest to oldest",
+  responses: {
+    200: {
+      description: "Getting succesfull",
+      content: {
+        "application/json": {
+          schema: z.array(toiletComment),
+        },
+      },
+    },
+    401: {
+      description: "Token Expired",
+    },
+    403: {
+      description: "Invalid token",
+    },
+    500: {
+      description: "Something went wrong",
+    },
+  },
+});
+
+registry.registerPath({
+  method: "get",
+  path: "/api/v1/toilet/{toiletId}/comments/{commentId}",
+  tags: ["Toilet"],
+  security: [{ bearerAuth: [] }],
+  description: "Deletes specific id comment of requesting user",
+  responses: {
+    204: {
+      description: "Deleting succesfull",
+    },
+    401: {
+      description: "Token Expired",
+    },
+    403: {
+      description: "Invalid token",
     },
     500: {
       description: "Something went wrong",
